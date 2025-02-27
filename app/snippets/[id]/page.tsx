a224900d9d4da13b1a20c982b7e524b321389e63
Linkon Siddique <linkon.sq48@gmail.com>
2025-02-28 00:03:13 +0600

    Fetch particular snippet

--- app/snippets/[id]/page.tsx
+++ app/snippets/[id]/page.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { notFound } from "next/navigation";
+import { db } from "@/db";
 
-const ShowSnippetPage = () => {
-  return <div>ShowSnippetPage</div>;
+interface ShowSnippetPageProps {
+  params: {
+    id: string;
+  };
+}
+
+const ShowSnippetPage = async (props: ShowSnippetPageProps) => {
+  const { id } = props.params;
+
+  const snippet = await db.snippet.findFirst({
+    where: {
+      id: parseInt(id),
+    },
+  });
+
+  if (!snippet) {
+    notFound();
+  }
+
+  return <div>{snippet.title}</div>;
 };
 
 export default ShowSnippetPage;
