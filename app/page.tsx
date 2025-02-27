6e906510dbe2570e7b400833e7afc3ad0da7a2c4
Linkon Siddique <linkon.sq48@gmail.com>
2025-02-27 23:46:12 +0600

    Fetch data with server components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Home Page</h1>
+import { db } from "@/db";
+
+export default async function Home() {
+  const snippets = await db.snippet.findMany();
+
+  const renderedSnippets = snippets.map((snippet) => (
+    <div key={snippet.id}>
+      <h3>{snippet.title}</h3>
+      <pre>{snippet.code}</pre>
     </div>
-  );
+  ));
+
+  return <div>{renderedSnippets}</div>;
 }
