b37a8225d342e75dff2f132a4a521841b9bae6ad
Linkon Siddique <linkon.sq48@gmail.com>
2025-04-02 14:39:12 +0600

    Show client component in a server component

--- app/snippets/[id]/edit/page.tsx
+++ app/snippets/[id]/edit/page.tsx
@@ -1,13 +1,24 @@
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+import SnippetEditForm from "@/comopnents/snippet-edit-form";
 interface SnippetEditPageProps {
-  params: {
+  params: Promise<{
     id: string;
-  };
+  }>;
 }
-
-const SnippetEditPage = ({ params }: SnippetEditPageProps) => {
-  const id = parseInt(params.id);
-
-  return <div>Editing snippet with id {id}</div>;
+const SnippetEditPage = async ({ params }: SnippetEditPageProps) => {
+  const { id } = await params;
+  const snippetId = parseInt(id);
+  const snippet = await db.snippet.findFirst({
+    where: { id: snippetId },
+  });
+  if (!snippet) {
+    return notFound();
+  }
+  return (
+    <div>
+      <SnippetEditForm snippet={snippet} />
+    </div>
+  );
 };
-
 export default SnippetEditPage;
